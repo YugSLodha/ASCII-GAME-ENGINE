68119a9c6b5d844c3a9f2c33914e4a5f977e2324
Yug Lodha <yuglodha81@gmail.com>
2025-11-23 11:02:31 +0530

    Improvements
    -Added a "build.bat" file for easier compiling
    -Added a Double Buffer System in the display
    -Added Dynamic cell_size calculation
    -Added A main loop

--- display.h
+++ display.h
@@ -5,15 +5,19 @@
 #include <vector>
 #include <iostream>
 
-extern int CELL_SIZE;
 extern int ROWS;
 extern int COLS;
 extern std::vector<std::wstring> grid;
+int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+int screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
+int CELL_SIZE = std::min(screenWidth / COLS, screenHeight / ROWS);
 static HFONT gFont = NULL;
+static HDC gMemDC = NULL;
+static HBITMAP gMemBitmap = NULL;
+static HBITMAP gOldBitmap = NULL;
 
-void CreateScaledFont(int cellSize)
-{
+void CreateScaledFont(int cellSize) {
     int fontHeight = (int)(cellSize * 0.9f);
 
     gFont = CreateFontW(
@@ -30,56 +34,60 @@ void CreateScaledFont(int cellSize)
     );
 }
 
-LRESULT CALLBACK DisplayWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
+LRESULT CALLBACK DisplayWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg)
     {
-    case WM_CREATE:
+    case WM_CREATE: {
         CreateScaledFont(CELL_SIZE);
+        HDC hdc = GetDC(hwnd);
+        gMemDC = CreateCompatibleDC(hdc);
+        gMemBitmap = CreateCompatibleBitmap(hdc, COLS * CELL_SIZE, ROWS * CELL_SIZE);
+        gOldBitmap = (HBITMAP)SelectObject(gMemDC, gMemBitmap);
+        ReleaseDC(hwnd, hdc);
         return 0;
+    }
 
-    case WM_PAINT:
-    {
+    case WM_PAINT: {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
-        SelectObject(hdc, gFont);
+        HBRUSH hBrush = (HBRUSH)GetStockObject(BLACK_BRUSH);
+        RECT rect = { 0, 0, COLS * CELL_SIZE, ROWS * CELL_SIZE };
+        FillRect(gMemDC, &rect, hBrush);
+        SelectObject(gMemDC, gFont);
+        SetBkMode(gMemDC, OPAQUE);
+        SetTextColor(gMemDC, RGB(255, 255, 255));
+        SetBkColor(gMemDC, RGB(0, 0, 0));
 
-        SetBkMode(hdc, OPAQUE);
-        SetTextColor(hdc, RGB(255, 255, 255));
-        SetBkColor(hdc, RGB(0, 0, 0));
-
-        for (int y = 0; y < ROWS; y++)
-        {
+        for (int y = 0; y < ROWS; y++) {
             for (int x = 0; x < COLS; x++)
             {
                 wchar_t ch[2] = { grid[y][x], 0 };
-
-                TextOutW(
-                    hdc,
-                    x * CELL_SIZE,
-                    y * CELL_SIZE,
-                    ch,
-                    1
-                );
+                TextOutW(gMemDC, x * CELL_SIZE, y * CELL_SIZE, ch, 1);
             }
         }
+        BitBlt(hdc, 0, 0, COLS * CELL_SIZE, ROWS * CELL_SIZE, gMemDC, 0, 0, SRCCOPY);
 
         EndPaint(hwnd, &ps);
         return 0;
     }
 
-    case WM_DESTROY:
+
+    case WM_DESTROY: {
+        if (gMemDC) {
+            SelectObject(gMemDC, gOldBitmap);
+            DeleteObject(gMemBitmap);
+            DeleteDC(gMemDC);
+        }
         DeleteObject(gFont);
         PostQuitMessage(0);
         return 0;
     }
-
+    }
     return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-HWND CreateDisplayWindow(HINSTANCE hInstance)
-{
+HWND CreateDisplayWindow(HINSTANCE hInstance) {
     const wchar_t CLASS_NAME[] = L"DisplayWindowClass";
 
     WNDCLASSW wc = {};
@@ -96,11 +104,11 @@ HWND CreateDisplayWindow(HINSTANCE hInstance)
     return CreateWindowExW(
         0,
         CLASS_NAME,
-        L"Display Grid (vector)",
-        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        width,
-        height,
+        L"Display Grid (Full Screen)",
+        WS_POPUP | WS_VISIBLE,
+        0, 0,
+        screenWidth,
+        screenHeight,
         NULL, NULL,
         hInstance,
         NULL
--- main.cpp
+++ main.cpp
@@ -1,43 +1,44 @@
 #include "display.h"
 
-int CELL_SIZE = 20;
-int ROWS = 15;
-int COLS = 30;
+int COLS = 96;
+int ROWS = 54;
 
 std::vector<std::wstring> grid;
+
 void InitGrid()
 {
-	grid.resize(ROWS);
-	for (int y = 0; y < ROWS; y++)
-	{
-		grid[y].resize(COLS);
+	grid.resize(ROWS, std::wstring(COLS, L' '));
 
-		for (int x = 0; x < COLS; x++)
-			grid[y][x] = L' ';
-	}
-
-	for (int x = 0; x < COLS; x++)
-	{
-		grid[0][x] = L'#';
-		grid[ROWS - 1][x] = L'#';
-	}
-	for (int y = 0; y < ROWS; y++)
+	std::fill(grid[0].begin(), grid[0].end(), L'#');
+	std::fill(grid[ROWS - 1].begin(), grid[ROWS - 1].end(), L'#');
+	for (int y = 1; y < ROWS - 1; y++)
 	{
 		grid[y][0] = L'#';
 		grid[y][COLS - 1] = L'#';
 	}
 }
 
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
+	InitGrid();
 	HWND hwnd = CreateDisplayWindow(hInstance);
 
 	MSG msg = {};
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+	bool running = true;
+
+	while (running) {
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			if (msg.message == WM_QUIT)
+				running = false;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
 	}
+
 
 	return 0;
 }
